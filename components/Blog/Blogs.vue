adcd031f3125265cf8862f525a037aa728eac2bb
ahmed reda <ahmed.reda@at-portal.info>
2023-10-25 01:43:10 +0300

    edit blog

--- components/Blog/Blogs.vue
+++ components/Blog/Blogs.vue
@@ -3,7 +3,7 @@
     <b-container>
       <h2 class="pt-3 mb-4">{{ $t('simple blog') }}</h2>
       <b-row>
-        <b-col lg="3" md="6" v-for="(num , index) in 4" :key="num">
+        <b-col lg="4" md="6" v-for="(num , index) in 3" :key="num">
           <BlogSimpleBlog :index="index" />
         </b-col>
       </b-row>
